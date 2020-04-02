d303106903278943d6bf41c39bbad055d79101b0
Ragnorok303 <jasontompkins22@gmail.com>
2020-04-01 18:23:11 -0600

    starting liri.js file

---
+++ liri.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+var keys = require("./key.js");
+
+let moment = require("moment");
+moment().format();
+var fs = require("fs");
+var axios = require("axios");
+var Spotify = require('node-spotify-api');
+
+
